e5cc4a93222684c07ac1ac6756fd4a12cef557cb
Alireza Esfahani <aynzad@gmail.com>
2024-01-28 10:04:32 +0100

    feat(genre): add genre page

---
+++ pages/genres/[uid].tsx
@@ -0,0 +1,75 @@
+import { GetStaticProps } from 'next'
+import { pagesRevalidate } from '@app/helpers/configs'
+import { createPrismicClient } from '@app/helpers/create-prismic-client'
+import { getImagesBlurData } from '@app/helpers/get-images-blur-data'
+import { getLayoutConfig } from '@app/helpers/get-layout-config'
+import { Genre, getLayout, Props } from '@app/partials/Genre'
+import { CollectionDocument } from '@app/prismicio-types'
+
+const GenrePage: NextPageWithLayout<Props> = props => {
+  return <Genre {...props} />
+}
+
+export const getStaticProps: GetStaticProps<Props> = async ({
+  previewData,
+  locale,
+  params
+}) => {
+  if (!params || !params.uid) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/'
+      }
+    }
+  }
+  const client = createPrismicClient({ previewData })
+
+  const genre = await client.getByUID('genre', params.uid as string, {
+    lang: locale,
+    fetchLinks: ['collection.title', 'collection.featuredImage']
+  })
+
+  const layoutConfig = await getLayoutConfig(client, locale, genre)
+
+  const collectionsImages = genre.data.collections.map(
+    item =>
+      (item.collection as unknown as CollectionDocument).data.featuredImage.long
+        .url || ''
+  )
+
+  console.log(collectionsImages)
+
+  const imagesBlurData = await getImagesBlurData([...collectionsImages])
+
+  return {
+    props: {
+      ...layoutConfig,
+      imagesBlurData,
+      genre
+    },
+    revalidate: pagesRevalidate
+  }
+}
+
+export async function getStaticPaths() {
+  const client = createPrismicClient()
+
+  const genres = await client.getAllByType('genre', {
+    lang: '*'
+  })
+
+  return {
+    paths: genres.map(genre => {
+      return {
+        params: { uid: genre.uid },
+        locale: genre.lang
+      }
+    }),
+    fallback: false
+  }
+}
+
+GenrePage.getLayout = getLayout
+
+export default GenrePage
---
+++ partials/Genre/index.tsx
@@ -0,0 +1,65 @@
+import { CollectionDocument } from '@app/prismicio-types'
+import { asText } from '@prismicio/helpers'
+import { PrismicText, SliceZone } from '@prismicio/react'
+import { CollectionCard } from '@app/components/CollectionCard'
+import Layout, { ILayoutConfig } from '@app/components/Layout'
+import { PageTitle } from '@app/components/PageTitle'
+import { components } from '@app/slices'
+import { GenreDocument } from '@app/types/custom-types'
+import classes from '../Home/home.module.css'
+
+interface GenreDocumentDataCollectionsItem {
+  collection: CollectionDocument
+}
+
+export interface Props extends WithImagesBlurData<ILayoutConfig> {
+  genre: GenreDocument
+}
+
+export const Genre: React.FC<Props> = ({ genre, settings, imagesBlurData }) => {
+  const collections = genre.data
+    .collections as GenreDocumentDataCollectionsItem[]
+
+  return (
+    <section className="container px-4">
+      <PageTitle subTitle={''}>
+        <PrismicText field={genre.data.title} />
+      </PageTitle>
+      <article className="md:container">
+        <SliceZone slices={genre.data.slices} components={components} />
+      </article>
+      <div className="container px-4 sm:mt-32">
+        {collections.length && (
+          <>
+            <section className={classes.collections}>
+              {collections.map(collection => (
+                <CollectionCard
+                  key={collection.collection.uid}
+                  collection={collection.collection}
+                  seeCollectionText={settings.data.seeCollection}
+                  isFloatTitle
+                  className="group relative"
+                  imagesBlurData={imagesBlurData}
+                />
+              ))}
+            </section>
+          </>
+        )}
+      </div>
+    </section>
+  )
+}
+
+export const getLayout: GetLayout<Props> = (page, pageProps) => {
+  return (
+    <Layout
+      title={asText(pageProps.genre.data.title)}
+      languages={pageProps.languages}
+      documentAlternateLanguages={pageProps.documentAlternateLanguages}
+      navigation={pageProps.navigation}
+      settings={pageProps.settings}
+    >
+      {page}
+    </Layout>
+  )
+}
